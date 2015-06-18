f0cf6b46f8829fe1252f0e1ca97eee485dbfa629
RindyanaPangestika <rindurindyana@gmail.com>
2015-06-18 13:11:19 +0700

    Update pengumuman.php

--- pengumuman.php
+++ pengumuman.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-md-12 img-rounded img-thumbnail" style="margin:20px 0px 20px 0px;">
                 <div class="row" style="background:#09F; margin-bottom:50px;">
-                	<h4 style="padding-left:10px;">Silakan isi formulir dibawah ini</h4>
+                	<h4 style="padding-left:10px;">Monggo isi formulir dibawah ini</h4>
                 </div>
                 <?php 
 					include_once 'hasil_seleksi.php';
